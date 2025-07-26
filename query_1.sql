b49be548d44d0f04e888c2830892a46067d937a1
Neckinho <radekneckar@seznam.cz>
2025-07-26 18:22:58 +0200

    Update query_1.sql
    Dodání SQL dotazu.

--- query_1.sql
+++ query_1.sql
@@ -1,1 +1,8 @@
 
+SELECT pib.name AS odvetvi, cp.payroll_year, AVG(cp.value) AS prumerna_mzda
+FROM czechia_payroll cp
+JOIN czechia_payroll_value_type pvt ON cp.value_type_code = pvt.code
+JOIN czechia_payroll_industry_branch pib ON cp.industry_branch_code = pib.code
+WHERE pvt.name = 'Průměrná hrubá mzda na zaměstnance'
+GROUP BY pib.name, cp.payroll_year
+ORDER BY pib.name, cp.payroll_year;
